46b4b81a37cb86c957316b1581b512fde8d537a3
wisteen <you@example.com>
2024-12-14 16:16:16 +0100

    webhook debug

--- proposal/views.py
+++ proposal/views.py
@@ -1,54 +1,59 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
-
 from .functions import *
 import json
-
 import logging
+
+# Configure logger
 logger = logging.getLogger(__name__)
 
 # Create your views here.
 
 def home(request):
-	return render(request, "proposal/index.html")
+    return render(request, "proposal/index.html")
 
 @csrf_exempt
 def WhatsAppWebhook(request):
-	if request.method == 'GET':
-		VERIFY_TOKEN='a6b53efd-a6fe-4025-99e7-3f2f225fd049'
-		mode= request.GET['hub.mode']
-		token = request.GET['hub.verify_token']
-		challenge = request.GET['hub.challenge']
-		if mode == 'subscribe' and token == VERIFY_TOKEN:
-			return HttpResponse(challenge, status=200)
-		else:
-			return HttpResponse('error', status=403)
-	if request.method == 'POST':
-		data = json.loads(request.body)
-		logger.info(f"Incoming POST Data: {data}")
-		if 'object' in data and 'entry' in data:
-			try:
-				for entry in data['entry']:
-					phoneNumber = entry['changes'][0]['value']['metadata']['display_phone_number']
-					phoneId = entry['changes'][0]['value']['metadata']['phone_number_id']
-					profileName = entry['changes'][0]['value']['contacts'][0]['profile']['name']
-					whatsAppId = entry['changes'][0]['value']['contacts'][0]['wa_id']
-					fromId = entry['changes'][0]['value']['messages'][0]['from']
-					messageId = entry['changes'][0]['value']['messages'][0]['id']
-					timestamp = entry['changes'][0]['value']['messages'][0]['timestamp']
-					text = entry['changes'][0]['value']['messages'][0]['body']
+    if request.method == 'GET':
+        VERIFY_TOKEN = 'a6b53efd-a6fe-4025-99e7-3f2f225fd049'
+        mode = request.GET.get('hub.mode')
+        token = request.GET.get('hub.verify_token')
+        challenge = request.GET.get('hub.challenge')
+        
+        if mode == 'subscribe' and token == VERIFY_TOKEN:
+            return HttpResponse(challenge, status=200)
+        else:
+            return HttpResponse('error', status=403)
 
-					phoneNumber = "2349125442676"
-					message = 'RE: {} was recieved from wisteenbecca'.format(text)
-					logger.info(f"Processing message from {profileName}: {text}")
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            logger.info(f"Incoming POST Data: {data}")
+            
+            if 'object' in data and 'entry' in data:
+                for entry in data['entry']:
+                    phoneNumber = entry['changes'][0]['value']['metadata']['display_phone_number']
+                    phoneId = entry['changes'][0]['value']['metadata']['phone_number_id']
+                    profileName = entry['changes'][0]['value']['contacts'][0]['profile']['name']
+                    whatsAppId = entry['changes'][0]['value']['contacts'][0]['wa_id']
+                    fromId = entry['changes'][0]['value']['messages'][0]['from']
+                    messageId = entry['changes'][0]['value']['messages'][0]['id']
+                    timestamp = entry['changes'][0]['value']['messages'][0]['timestamp']
+                    text = entry['changes'][0]['value']['messages'][0]['body']
 
-					sendWhatsappMessage(phoneNumber, message)
+                    phoneNumber = "2349125442676"
+                    message = 'RE: {} was received from wisteenbecca'.format(text)
+                    logger.info(f"Processing message from {profileName}: {text}")
 
-			except json.JSONDecodeError as e:
-            	logger.error(f"JSON decoding error: {e}")
-            	return HttpResponse('Invalid JSON', status=400)
-        	except Exception as e:
-            	logger.error(f"Error processing webhook: {e}")
-            	return HttpResponse('Error processing request', status=500)
-	return HttpResponse('success', status=200)
+                    sendWhatsappMessage(phoneNumber, message)
+
+        except json.JSONDecodeError as e:
+            logger.error(f"JSON decoding error: {e}")
+            return HttpResponse('Invalid JSON', status=400)
+
+        except Exception as e:
+            logger.error(f"Error processing webhook: {e}")
+            return HttpResponse('Error processing request', status=500)
+
+    return HttpResponse('success', status=200)
